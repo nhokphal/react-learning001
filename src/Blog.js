2d7fdeb072340896db381697e5a2616fe5c45d79
nhokphal <phalnhok@gmail.com>
2020-08-29 15:00:24 +0700

    2 files modified, 3 added, 1 deleted

--- src/Blog.js
+++ src/Blog.js
@@ -13,8 +13,7 @@ import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+
 
 const useStyles = makeStyles((theme) => ({
   mainGrid: {
